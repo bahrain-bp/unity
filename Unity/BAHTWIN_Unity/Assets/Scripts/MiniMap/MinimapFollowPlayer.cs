00df9a4caba2fa82fd91880b5507465982f3d147
Manar Alsetrawi <202200412@student.polytechnic.bh>
2025-12-03 02:15:01 +0300

    Added the character following arrow for the mini map and made it dynamic

--- Unity/BAHTWIN_Unity/Assets/Scripts/MiniMap/MinimapFollowPlayer.cs
+++ Unity/BAHTWIN_Unity/Assets/Scripts/MiniMap/MinimapFollowPlayer.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 public class MinimapFollowPlayer : MonoBehaviour
 {
-    public Transform target;
-    public float height = 40f;
+       public Transform target;   
+    public float height = 40f; 
 
     void LateUpdate()
     {
         if (!target) return;
 
-        Vector3 pos = target.position;
-        pos.y = height;
-        transform.position = pos;
+        // Follow player X and Z positions, keep Y constant
+        Vector3 newPos = target.position;
+        newPos.y = height;
+
+        transform.position = newPos;
 
         // If you want the map fixed, keep rotation (90,0,0)
         // If you want it to rotate with the player, uncomment:
-        transform.rotation = Quaternion.Euler(90f, target.eulerAngles.y, 0f);
+        //transform.rotation = Quaternion.Euler(90f, target.eulerAngles.y, 0f);
     }
 }
 
