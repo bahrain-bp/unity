20e7ee9e63fcdbcd5e22b392dcff214429d4366b
HusainMo <blksnp95@gmail.com>
2025-12-07 03:45:00 +0300

    Signout Button

--- frontend/src/config/auth.ts
+++ frontend/src/config/auth.ts
@@ -1,7 +1,7 @@
 export const AwsConfigAuth = {
   Auth: {
     Cognito: {
-      userPoolId: "us-east-1_pcnCiDaXa",
+      userPoolId: import.meta.env.VITE_APP_AUTH_USER_POOL_ID,
       userPoolClientId: import.meta.env.VITE_APP_AUTH_USER_POOL_WEB_CLIENT_ID,
       loginWith: {
         email: true,
