9f82b39fb5bdd1b0e3c4074db1e29d5b334955ae
Zainab Hammad <zainabhammaad@gmail.com>
2025-12-13 08:58:40 +0300

    Remove old/unneeded lambda files

--- lambda/finalizeImage.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZmluYWxpemVJbWFnZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImZpbmFsaXplSW1hZ2UudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbIiJdfQ==
