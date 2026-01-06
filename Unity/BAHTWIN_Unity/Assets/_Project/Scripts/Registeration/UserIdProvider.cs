cbd2eedb98e5408ce6b013f2648332e4cdbe064d
khabdulla198 <khadijahasan198@gmail.com>
2026-01-06 14:09:01 +0300

    fixed the userID Provider Script

--- Unity/BAHTWIN_Unity/Assets/_Project/Scripts/Registeration/UserIdProvider.cs
+++ Unity/BAHTWIN_Unity/Assets/_Project/Scripts/Registeration/UserIdProvider.cs
@@ -6,17 +6,26 @@ public class UserIdProvider : MonoBehaviour
     [SerializeField] private string editorTestUserId = "";
     [SerializeField] private string editorTestIdToken = "EDITOR_MOCK_TOKEN";
 
-    [Header("Browser Storage Keys")]
+    [Header("Browser Storage Keys (must match website localStorage keys)")]
     [SerializeField] private string userIdKey = "userId";
     [SerializeField] private string idTokenKey = "idToken";
 
     public string GetUserId()
     {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        return WebGLLocalStorage.GetString(userIdKey);
+#else
+        // Editor/Standalone testing
         return PlayerPrefs.GetString(userIdKey, editorTestUserId);
+#endif
     }
 
     public string GetIdToken()
     {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        return WebGLLocalStorage.GetString(idTokenKey);
+#else
         return PlayerPrefs.GetString(idTokenKey, editorTestIdToken);
+#endif
     }
 }
