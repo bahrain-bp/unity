f372e1110ec0cc799f5dea4247ef6b8677b9dda7
Zainab Hammad <zainabhammaad@gmail.com>
2026-01-11 07:02:20 +0300

    add alexa stack to cdk app entry point

--- bin/my-cdk-app.ts
+++ bin/my-cdk-app.ts
@@ -12,6 +12,8 @@ import { IoTStack } from "../lib/IoTStack";
 import { UnityWebSocketStack } from "../lib/unity-websocket-stack";
 import { BuildUploadStack } from "../lib/BuildUploadStack"; 
 
+import { AlexaStack } from "../lib/AlexaStack";
+
 const app = new cdk.App();
 
 const env = {
@@ -75,6 +77,17 @@ new APIStack(app, "Unity-APIStack", {
   wsStack,
   env,
 });
+
+// 7) Alexa Echo Show stack
+const alexaStack = new AlexaStack(app, "Unity-AlexaStack", {
+  dbStack,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION || "us-east-1",
+  },
+});
+
+alexaStack.addDependency(dbStack);
 
 
 // const FRStack = new FacialRecognitionStack(app, 'FacialRecognitionStack', {
